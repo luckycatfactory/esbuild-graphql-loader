8ff27651e9481c6fefd1524f8eb5b3a53949edcf
Louis Cruz <louisstephancruz@me.com>
2020-12-29 13:07:22 -0800

    fix(index.ts): prevent stripping of undefined from serialized DocumentNode
    JSON.stringify is used to produce a string of the AST. Unfortunately, that meant that descriptions,
    which are sometimes undefined, were getting stripped from the AST objects. That unlikely would have
    any impact on any users of the package, but out of principle we shouldn't be removing altering the
    serialized AST in any way by default. This commit also improves the testing tooling.
    
    BREAKING CHANGE: Previously, the definition property on ASTNodes would be removed if it had a value
    of undefined. This change makes it such that the property will exist with a value of undefined. Any
    code relying on the number of properties or the existence of the definition key to exist could begin
    breaking as a result of this change.

---
+++ tests/cases/custom-regex-regex/custom_regex-case.test.ts
@@ -0,0 +1,18 @@
+import path from "path";
+
+import {
+  getJSONDocumentNodeFromString,
+  importFileAsString,
+} from "../utilities";
+import output from "./output";
+
+describe("custom regex", () => {
+  it("generates the expected output", () => {
+    const targetFile = path.join(__dirname, "./target.gql");
+    return importFileAsString(targetFile).then((data) => {
+      const expected = getJSONDocumentNodeFromString(data);
+
+      expect(output).toEqual(expected);
+    });
+  });
+});
---
+++ tests/cases/operation-case/operation-case.test.ts
@@ -0,0 +1,18 @@
+import path from "path";
+
+import {
+  getJSONDocumentNodeFromString,
+  importFileAsString,
+} from "../utilities";
+import output from "./output";
+
+describe("operations", () => {
+  it("generates the expected output", () => {
+    const targetFile = path.join(__dirname, "./target.graphql");
+    return importFileAsString(targetFile).then((data) => {
+      const expected = getJSONDocumentNodeFromString(data);
+
+      expect(output).toEqual(expected);
+    });
+  });
+});
