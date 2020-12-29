6df2e580bc59969ddf1ad470c55e48bb9e8c2938
Louis Cruz <louisstephancruz@me.com>
2020-12-29 13:18:13 -0800

    docs(readme.md): improve documentation

--- tests/cases/operation-case/output.js
+++
@@ -1,8 +0,0 @@
-// tests/cases/operation-case/target.graphql
-var target_default = {kind: "Document", definitions: [{kind: "OperationDefinition", operation: "query", name: {kind: "Name", value: "getPosts"}, variableDefinitions: [], directives: [], selectionSet: {kind: "SelectionSet", selections: [{kind: "Field", alias: void 0, name: {kind: "Name", value: "posts"}, arguments: [], directives: [], selectionSet: {kind: "SelectionSet", selections: [{kind: "Field", alias: void 0, name: {kind: "Name", value: "id"}, arguments: [], directives: [], selectionSet: void 0}]}}]}}], loc: {start: 0, end: 40}};
-
-// tests/cases/operation-case/index.ts
-var operation_case_default = target_default;
-export {
-  operation_case_default as default
-};
