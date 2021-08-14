4d38cc6df1a20611e2471a1c29eece96ff84859d
Mark Martirosian <hello@markmartirosian.com>
2021-08-14 10:40:58 +0400

    amend exports
    export `generateGraphQLString` and `generateContentsFromGraphqlString`

--- src/index.ts
+++ src/index.ts
@@ -116,7 +116,7 @@ const parseGraphQLFile = (filePath: string): Promise<ParsedGraphQLFile> =>
     });
   });
 
-const generateGraphQLString = (entryPointPath: string): Promise<string> => {
+export const generateGraphQLString = (entryPointPath: string): Promise<string> => {
   const cache: Record<string, ParsedGraphQLFile> = {};
   const parsePromises: Promise<null>[] = [];
   const seenImportPaths = new Set();
@@ -218,7 +218,7 @@ const collectAllFragmentReferences = (
   return references;
 };
 
-const generateContentsFromGraphqlString = (
+export const generateContentsFromGraphqlString = (
   graphqlString: string,
   mapDocumentNode?: (documentNode: DocumentNode) => DocumentNode
 ): string => {
